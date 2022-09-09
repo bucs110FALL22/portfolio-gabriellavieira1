861c878af0c9701dfd4fcdb793699f55e8487367
Gabriella Vieira <112492409+gabriellavieira1@users.noreply.github.com>
2022-09-09 19:39:02 +0000

    I completed Lab 2

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -6,5 +6,28 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = (10)
+print("Classes per week:", classes_per_week)
+
+cost_per_class = (cost_per_week/classes_per_week)
+
+
+
+print("Weeks:", weeks)
+
+
+print("Classes:", classes)
+
+
+print("Tuition:", tuition)
+
+
+print("Finished result of the cost per class:", cost_per_class)
+
 
 #Part B
+import random
+First = ("mylist")
+mylist = [2,3,6,7,8]
+Second = (random.choice(mylist))
+print(Second)
