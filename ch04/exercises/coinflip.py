261a4cd3935bc862d843eafbebaf87dbd4298dbc
Gabriella Vieira <112492409+gabriellavieira1@users.noreply.github.com>
2022-10-10 20:58:38 +0000

    I completed Ch.4 exercise coinflip.

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -3,29 +3,34 @@ import random
 
 leo = turtle.Turtle()
 wn = turtle.Screen()
-wn.bgcolor("blue")
+wn.bgcolor("white")
 
 leo.color("green")
 leo.shape("turtle")
+leo.speed(0)
 
-leo.up()
-leo.goto(0,0) 
+distance = 10
+angle = 90
+in_screen = True
 
-def isinscreen(wn, leo):
-  if random.random() > 0.1:
-    return True
-  else:
-    return False
+colors = ["green", "blue", "purple"]
+while in_screen:
+  coin = random.randrange(0,2)
+  if coin==0:    #Heads
+      leo.left(angle)
+  else:     #Tails
+      leo.right(angle)
+      leo.fd(distance)
 
+x_range = wn.window_width()/2
+y_range = wn.window_height()/2
+turtleX =leo.xcor()
+turtleY = leo.ycor()
 
-while isinscreen(wn, leo):
-  num = random.randrange(2)
-  if num == 0:   
-   (leo.left(90))
-else:
-   (leo.right(90))
+leo.colors(colors[0])
+colors.append(colors.pop(0))
 
-leo.forward(50)
-
+if abs(turtleX) > x_range or abs(turtleY) > y_range:
+  in_screen = False
 
 wn.exitonclick()
