b090261f10ce9ea6477db5a14010e6924589afdf
Gabriella Vieira <112492409+gabriellavieira1@users.noreply.github.com>
2022-10-15 03:21:04 +0000

    Attempted Ch.5 lab

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,58 +1,94 @@
 #Part A
-
-#def count_a():
-n=0
-while (n <101):
-  print("The loop value is: ", n)
-  n= n+1
-  print(n)
-
-#def count_a()
+import pygame
 
 
-
-#def count2_a(x)
-c=0
-while (c < 101):
-    if c%2 ==0:
-        c = c//2
-    else:
-        c = c*3 +1
-        print(c)
-#print count2_a(101) n!=1
-#def count2_a()
-
-
-
-#Part B
-#def count_b():
-count=0
-n=0
-while (n <101):
-  print("The loop value is: ", n)
-  n= n+1
-  count= count + 1
-  print(count)
-  print(n)
-
-#def count_b()
-
-
-
-#def count2_b(x)
-n=0
-count=0
-while (n < 101):
+n=101
+while (n != 1 ): 
   if n%2 ==0:
-    n = n//2
-    count= count +1
-    print(count)
-  else:
-    n = n*3 +1
-    count = count +1
-    print(count)
-    print(n)
-#print count2_a(101)
-#def count2_b()
+     n = n//2
+     print (n)
+  else: 
+     n = n*3 +1
+     print(n)
 
-#Part B Checking a Range
+
+
+
+#Part B: Refactoring to Track a Range of Iterations
+n=101
+count=0
+while (n != 1):
+    if n%2 ==0:
+       n = n//2
+    else: 
+       n = n*3 +1
+    count +=1
+    print ("Count: ", count)
+
+
+#Part B: Checking a Range
+start = (n)
+n=101
+upper_limit=20 
+#list=[]
+iters ={}
+for start in range(2, upper_limit +1 ,1):
+  count=0
+  while (n != 1): 
+      if n%2 ==0:
+        n = n//2
+      else: 
+        n = n*3 +1
+        
+        count +=1
+      
+        iters[start] = count
+      #iters= {n:count}
+      #list.append(iters)
+        print(iters)
+#print(list)
+
+#Part C
+pygame.init()
+window= pygame.display.set_mode()
+window.fill("white")
+pygame.display.flip()
+
+font= pygame.font.Font(None,50)
+pygame.font.init()
+
+upper_limit=20
+iters={}
+max_so_far=0
+max_val =25
+scale=5
+
+
+
+for n in range(2, upper_limit +1 ,1):
+  n=101
+  count=0
+  while (n != 1): 
+      if n%2 ==0:
+        n = n//2
+      else: 
+        n = n*3 +1
+        count+=1
+        iters[start] = count
+        print(iters)
+        iters_items=(iters,count)
+
+coords =(iters_items)
+if coords in range(2):
+  pygame.draw.lines(window, "blue", False, coords)
+  pygame.transform.flip()
+
+elif max_so_far< count:
+  max_so_far +=1
+  max_val +=1
+msg=font.render("Largest number of iterations", True, "blue")
+#pygame.Surface.blit(msg, (10,10))
+#pygame.transform.flip()
+
+pygame.display.flip()
+
