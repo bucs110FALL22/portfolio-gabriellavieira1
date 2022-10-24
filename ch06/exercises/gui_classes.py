3612887c188747830a69291f0d558039dd260290
Gabriella Vieira <112492409+gabriellavieira1@users.noreply.github.com>
2022-10-24 21:57:28 +0000

    I completed Ch. 6 gui classes

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,18 @@
+class Mario:
+  def __init__(self):
+    self.jumps= True
+    self.can_lose_game_lives= True
+    self.moves_directions= True
+
+class Stepping_block:
+  def __init__(self):
+    self.is_high_up = True
+    self.mysteryboxes = 1
+    self.is_long_horizontally = False
+    
+class Tube:
+  def __init__(self):
+    self.is_green = True
+    self.is_wide = True
+    self.is_on_ground = True
+    
