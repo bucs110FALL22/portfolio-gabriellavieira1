a032867a0c219e4bbb07a74f707b44f742dd4412
Gabriella Vieira <112492409+gabriellavieira1@users.noreply.github.com>
2022-10-28 20:43:14 +0000

    I finished the exam1/midterm as best as I could

--- ch06/Exam/exam.py
+++ ch06/Exam/exam.py
@@ -1,13 +1,18 @@
 import turtle
 
 def drawSquare(lily, num_sides, side_length):
-  loops=4
+  '''
+   general function description: Creates square
+   args: Args are lily the arrow, num_sides is the amount of sides used, and side_length is what makes the object long or short. All are types of strings
+   return:  Loops is used to make the for loop run a specific amount and its type is an integer.
+  '''
+  loops=0
   lily.penup()
   lily.goto(-200,200)
   lily.pendown()
   lily.begin_fill()
   lily.fillcolor("white")
-  for i in range (loops):
+  for i in range (loops,4):
         lily.pencolor("black")
         lily.forward(side_length)
         lily.right(360/num_sides)
@@ -16,7 +21,11 @@ def drawSquare(lily, num_sides, side_length):
   return(loops)
 
 def theCircle(lily):
-
+  '''
+   general function description: Creates the circle for the emoji 
+   args: Lily is the arrow and is a string function
+   return:  Lily is the arrow and is a string function 
+  '''
   
   lily.penup()
   lily.goto(0,0)
@@ -30,8 +39,11 @@ def theCircle(lily):
   return(lily)
   
 def theEyes(lily):
-
-  
+  '''
+   general function description: Creates the eyes
+   args: Lily is the arrow and is a string function
+   return: Lily is the arrow and is a string function 
+  '''
   lily.goto(50, 120)
   lily.pendown()
   lily.pencolor("black")
@@ -47,7 +59,11 @@ def theEyes(lily):
   return(lily)
   
 def theOpenmouth(lily):
-
+  '''
+   general function description: Creates the mouth
+   args: Lily is the arrow whose type is a string
+   return:Lily is the arrow whose type is a string
+  '''
   
   lily.penup()
   lily.goto(2, 40)
@@ -59,8 +75,11 @@ def theOpenmouth(lily):
   return (lily)
   
 def drawSick(lily):
- 
-  
+  '''
+   general function description: This creates the appeareance of throwing up
+   args: Lily in this case is the turtle whose type is a string
+   return:Lily is the turtle whose type is a string
+  '''
   lily.pendown()
   lily.pencolor("green")
   lily.pensize(50)
