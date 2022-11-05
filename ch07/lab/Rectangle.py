9ee768667e8e5cef0e130765fc2857e0e7dfd196
Gabriella Vieira <112492409+gabriellavieira1@users.noreply.github.com>
2022-11-05 03:33:45 +0000

    Completed Rectangle

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -16,8 +16,6 @@ def __str__(self):
   general function: Creates a string that contains the x, y, height, and width arguments that is then returned
   args: self 
   return: result_str
-  
-  
   '''
   result_str = f" x is {self.x}, y is{self.y}, height is{self.height}, width is{self.width}"
   return(result_str)
